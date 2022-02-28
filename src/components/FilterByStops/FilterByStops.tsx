4f92badaab8d0cef4b62322d401b733797efddd4
Mrcrabus <bodenchyck97@mail.ru>
2022-02-28 20:54:03 +0300

    Begin radio

--- src/components/FilterByStops/FilterByStops.tsx
+++ src/components/FilterByStops/FilterByStops.tsx
@@ -4,28 +4,37 @@ import {Container, Card, Form} from "react-bootstrap";
 const FilterByStops = () => {
     return (
         <Card
-            className={'d-flex flex-column justify-content-center align-items-center border'}
+            className={'d-flex flex-column justify-content-center align-items-center '}
             style={{width: 232, height: 227}}
 
         >
             Количество пересадок
             <Form>
-                {['checkbox', 'radio'].map((type) => (
-                    <div key={`default-${type}`} className="mb-3">
-                        <Form.Check
-                            type={type}
-                            id={`default-${type}`}
-                            label={`default ${type}`}
-                        />
 
-                        <Form.Check
-                            disabled
-                            type={type}
-                            label={`disabled ${type}`}
-                            id={`disabled-default-${type}`}
-                        />
-                    </div>
-                ))}
+                <div key={`default-checkbox`} className="mb-3">
+                    <Form.Check
+                        type={'checkbox'}
+                        id={`default-checkbox`}
+                        label={`Без пересадок`}
+                    />
+                    <Form.Check
+                        type={'checkbox'}
+                        id={`default-checkbox`}
+                        label={`1 пересадка`}
+                    />
+                    <Form.Check
+                        type={'checkbox'}
+                        id={`default-checkbox`}
+                        label={`2 пересадки`}
+                    />
+                    <Form.Check
+                        type={'checkbox'}
+                        id={`default-checkbox`}
+                        label={`3 пересадки`}
+                    />
+
+                </div>
+
             </Form>
 
 
