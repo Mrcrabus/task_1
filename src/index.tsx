2676815cfb67f94c04ebd2b88a6ba9e4f7f5a7b1
Mrcrabus <bodenchyck97@mail.ru>
2022-04-05 16:15:32 +0300

    Items fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {store} from './store'
+import {Provider} from 'react-redux'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
